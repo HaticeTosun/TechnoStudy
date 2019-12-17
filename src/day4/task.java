28dd64f570d7136f2a8cc61e4a2e61a4816da1bc
HaticeTosun <56235635+HaticeTosun@users.noreply.github.com>
2019-12-16 22:23:13 -0500

    task.java

---
+++ src/day4/task.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class task {
+    public static void main(String[] args) {
+        int num1 = 45;
+        int num2 = 899;
+        TestComparingOperators(num1, num2);
+    }
+      private static void TestComparingOperators(int number1, int number2){
+        if(number1==number2){
+            System.out.println("Numbers are equal");
+        }
+        else{
+            System.out.println("Numbers are not egual");
+        }
+      }
+    }
+
